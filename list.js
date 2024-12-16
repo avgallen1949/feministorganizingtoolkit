32e545598b3e8bb0224ca1c02c5c557dda4c5c75
avgallen1949 <138723887+avgallen1949@users.noreply.github.com>
2024-12-16 18:23:38 -0500

    Add files via upload

--- list.js
+++ list.js
@@ -1,69 +1,62 @@
-$("#filter").on("click", "a[filter]", function(){
-  
-    $("#filter a").removeAttr("selected");
-    let what = $(this).attr("filter");
-    $(this).attr("selected", "");  
-    
-    if (what == "all") {
-      $("#_list .r").show();
-    } else {
-      $("#_list .r").show();
-      $("#_list .r:not([type='" + what + "'])").hide();
-    }
-  
-    
-  });
 
-  
-  
   const SPREADSHEET_ID = "1lQ7or3ZUrOArjcqTj73PRoTlE1fheuvKXDI8F0GaO94";
   const TAB_NAME = "Sheet1";
-  
-  
-    $(document).ready(function () {
-      
-      
-        $.getJSON("https://opensheet.elk.sh/" + SPREADSHEET_ID + "/" + TAB_NAME, function (data) {
-      
-      console.log(data); 
-      
-       // go over everything in data and run the code below
-      data.forEach(function (entry, index) {
-        
-        let _links = "";
-        
-        if(entry["mirror/0"] && entry["mirror/0"].length !== 0){   _links += `<a href='`+entry["mirror/0"]+` target="_blank'> </a>`; }
 
-        
-        let d = $(`<div class='r' type='` + (entry.type).toLowerCase() + `'> 
-            <a href="` + entry["mirror/0"] + `" target="_blank" class="_link">
-              <div class="_title">
-              
-              <h2 title>`
-              + entry.title +     
-              `</h2></div>
-              
-            </div>`)
-          .appendTo("#_list");
-        
+$(document).ready(function () {
 
-    
-      });
-          
-      // update counters
-  
-      $("[counter]").text($(".r").length);
-      $(".count_all").text($(".r").length);
-      $(".count_abortion").text($(".r[type*='abortion']").length);
-      $(".count_education").text($(".r[type*='education']").length);
-      $(".count_org").text($(".r[type*='org']").length);
-      $(".count_organization").text($(".r[type*='organization']").length);
-    });  
-  
-  
-  
-      
-      // write
-      
-  
-    })
+  // Fetch data from the spreadsheet
+  $.getJSON("https://opensheet.elk.sh/" + SPREADSHEET_ID + "/" + TAB_NAME, function (data) {
+
+    console.log(data);
+
+    // Clear any previous data and add new entries
+    $("#_list").empty(); // Clear existing list items before adding new ones
+
+    // Go over everything in data and run the code below
+    data.forEach(function (entry) {
+      let _links = "";
+      if (entry["mirror/0"] && entry["mirror/0"].length !== 0) { 
+        _links += `<a href='` + entry["mirror/0"] + `' target="_blank"> </a>`; 
+      }
+
+      // Dynamically create .r elements with the correct type
+      let d = $(`<div class='r' type='` + (entry.type).toLowerCase() + `'> 
+        <a href="` + entry["mirror/0"] + `" target="_blank" class="_link">
+          <div class="_title">
+            <h2 title>` + entry.title + `</h2>
+          </div>
+        </a>
+      </div>`).appendTo("#_list");
+    });
+
+    // Update counters
+    $(".count_all").text($(".r").length);
+    $(".count_abortion").text($(".r[type*='abortion']").length);
+    $(".count_education").text($(".r[type*='education']").length);
+    $(".count_org").text($(".r[type*='org']").length);
+    $(".count_organization").text($(".r[type*='organization']").length);
+
+    // Apply filter behavior
+    $("#filter").on("click", "a[filter]", function () {
+      // Remove the 'selected' class from all links
+      $("#filter a").removeClass("selected");
+
+      // Mark the clicked link as selected
+      $(this).addClass("selected");
+
+      let filterValue = $(this).attr("filter");
+
+      if (filterValue == "all") {
+        // Show all items when 'all' is selected
+        $("#_list .r").show();
+      } else {
+        // Hide all items first
+        $("#_list .r").hide();
+
+        // Show only items that match the selected filter
+        $("#_list .r[type='" + filterValue + "']").show();
+      }
+    });
+  });
+
+});
